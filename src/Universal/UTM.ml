bab15143b861f30f27ef346ac6e1127848f12a5b
Stephen Andre <andreste@f201-14.e-im2ag.ujf-grenoble.fr>
2019-04-03 09:30:53 +0200

    copie dégueu

--- src/Universal/UTM.ml
+++ src/Universal/UTM.ml
@@ -257,16 +257,16 @@ let utm: Turing_Machine.t =
 	(* Finit sur W *)
 	in
 	let rechTrans_transitions = [
-        (rechTransD, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpEtatD);
+    (rechTransD, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpEtatD);
 
-        (rechTrans1, Action(Nop), cmpEtatD);
-        (cmpEtatA, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpReadD);
-        (cmpEtatE, Action(Nop), prchTransD);
+    (rechTrans1, Action(Nop), cmpEtatD);
+    (cmpEtatA, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpReadD);
+    (cmpEtatE, Action(Nop), prchTransD);
 
-        (cmpReadA, Action(Nop), rechTransA);
-        (cmpReadE, Action(Nop), prchTransD);
+    (cmpReadA, Action(Nop), rechTransA);
+    (cmpReadE, Action(Nop), prchTransD);
 
-        (prchTransA, Action(Nop), rechTransD)
+    (prchTransA, Action(Nop), rechTransD)
 	]
 
 	(* Commence sur W *)
@@ -286,18 +286,18 @@ let utm: Turing_Machine.t =
 	(* Finit sur C et au début de B3 *)
 	in
 	let chEtat_transitions = [
-        (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Acc), No_Write, Right); Nop]), accept);
-        (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Exc), No_Write, Right); Nop]), reject);
-        (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Std), No_Write, Right); Nop]), chEtat1);
+    (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Acc), No_Write, Right); Nop]), accept);
+    (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Exc), No_Write, Right); Nop]), reject);
+    (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Std), No_Write, Right); Nop]), chEtat1);
 
-        (chEtat1, Action(Simultaneous [Nop; RWM(Match(BUT S), No_Write, Right); Nop]), reject);
-        (chEtat1, Action(Simultaneous [Nop; RWM(Match(VAL S), No_Write, Right); Nop]), chEtat2);
+    (chEtat1, Action(Simultaneous [Nop; RWM(Match(BUT S), No_Write, Right); Nop]), reject);
+    (chEtat1, Action(Simultaneous [Nop; RWM(Match(VAL S), No_Write, Right); Nop]), chEtat2);
 
-        (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL Z), No_Write, Right); RWM(Match ANY, Write Z, Right)]), chEtat2);
-        (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL U), No_Write, Right); RWM(Match ANY, Write U, Right)]), chEtat2);
-        (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL C), No_Write, Here); Nop]), chEtatR);
+    (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL Z), No_Write, Right); RWM(Match ANY, Write Z, Right)]), chEtat2);
+    (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL U), No_Write, Right); RWM(Match ANY, Write U, Right)]), chEtat2);
+    (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL C), No_Write, Here); Nop]), chEtatR);
 
-        (chEtatR, Run_on(TM_Basic.left_most, [3]), chEtatA)
+    (chEtatR, Run_on(TM_Basic.left_most, [3]), chEtatA)
 	]
 
 	in
@@ -334,16 +334,71 @@ let utm: Turing_Machine.t =
           (* ... à compléter ... *)
 
         ] *)
-			init_transitions @
+			(* init_transitions @
 			loop_transitions @
 			rechTrans_transitions @
 			prchTrans_transitions @
 			cmpEtat_transitions @
 			cmpRead_transitions @
 			excTrans_transitions @
-			chEtat_transitions @
-			loop_transitions @
-			init_transitions
+			chEtat_transitions *)
+      (prchTransD, Action( Simultaneous [ Nop ; RWM(Match(OUT [C; B]), No_Write, Right) ; Nop ] ), prchTransD) ;
+      (prchTransD, Action( Simultaneous [ Nop ; RWM(Match(VAL B), No_Write, Here) ; Nop ] ), reject) ;
+      (prchTransD, Action( Simultaneous [ Nop ; RWM(Match(VAL C), No_Write, Right) ; Nop ] ), prchTrans_o) ;
+      (prchTrans_o, Action( Simultaneous [ Nop ; RWM(Match(OUT [O ;B]), No_Write, Right) ; Nop ] ), prchTransD) ;
+      (prchTrans_o, Action( Simultaneous [ Nop ; RWM(Match(VAL B), No_Write, Here) ; Nop ] ), reject) ;
+      (prchTrans_o, Action( Simultaneous [ Nop ; RWM(Match(VAL O), No_Write, Right) ; Nop ] ), prchTransA) ;
+
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL Z), No_Write, Right) ; RWM(Match(VAL Z), No_Write, Right) ] ), cmpEtatD) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL U), No_Write, Right) ; RWM(Match(VAL U), No_Write, Right) ] ), cmpEtatD) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL C), No_Write, Right) ; RWM(Match(VAL B), No_Write, Left) ] ), cmpEtatA_r) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL U), No_Write, Here) ; RWM(Match(VAL Z), No_Write, Here) ] ), cmpEtatE_r) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL Z), No_Write, Here) ; RWM(Match(VAL U), No_Write, Here) ] ), cmpEtatE_r) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(VAL C), No_Write, Here) ; RWM(Match(BUT B), No_Write, Here) ] ), cmpEtatE_r) ;
+      (cmpEtatD, Action( Simultaneous [ Nop ; RWM(Match(BUT C), No_Write, Here) ; RWM(Match(VAL B), No_Write, Left) ] ), cmpEtatE_r) ;
+      (cmpEtatA_r, Parallel [ Action(Nop) ; Action(Nop) ; Run(TM_Basic.left_most) ], cmpEtatA) ;
+      (cmpEtatE_r, Parallel [ Action(Nop) ; Action(Nop) ; Run(TM_Basic.left_most) ], cmpEtatE) ;
+
+      (cmpReadD, Action( Simultaneous [ RWM(Match(VAL Z), No_Write, Here) ; RWM(Match(VAL Z), No_Write, Right) ; Nop ] ), cmpReadA) ;
+      (cmpReadD, Action( Simultaneous [ RWM(Match(VAL U), No_Write, Here) ; RWM(Match(VAL U), No_Write, Right) ; Nop ] ), cmpReadA) ;
+      (cmpReadD, Action( Simultaneous [ RWM(Match(VAL B), No_Write, Here) ; RWM(Match(VAL B), No_Write, Right) ; Nop ] ), cmpReadA) ;
+      (cmpReadD, Action( Simultaneous [ RWM(Match(BUT Z), No_Write, Here) ; RWM(Match(VAL Z), No_Write, Right) ; Nop ] ), cmpReadE) ;
+      (cmpReadD, Action( Simultaneous [ RWM(Match(BUT U), No_Write, Here) ; RWM(Match(VAL U), No_Write, Right) ; Nop ] ), cmpReadE) ;
+      (cmpReadD, Action( Simultaneous [ RWM(Match(BUT B), No_Write, Here) ; RWM(Match(VAL B), No_Write, Right) ; Nop ] ), cmpReadE) ;
+
+      (rechTransD, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpEtatD);
+      (rechTrans1, Action(Nop), cmpEtatD);
+      (cmpEtatA, Action(Simultaneous [Nop; RWM(Match ANY, No_Write, Right); Nop]), cmpReadD);
+      (cmpEtatE, Action(Nop), prchTransD);
+      (cmpReadA, Action(Nop), rechTransA);
+      (cmpReadE, Action(Nop), prchTransD);
+      (prchTransA, Action(Nop), rechTransD) ;
+
+      (excTransD, Action( Simultaneous [ RWM(Match ANY, Write Z, Here) ; RWM(Match(VAL Z), No_Write, Right) ; Nop ]), excTrans_m) ;
+      (excTransD, Action( Simultaneous [ RWM(Match ANY, Write U, Here) ; RWM(Match(VAL U), No_Write, Right) ; Nop ]), excTrans_m) ;
+      (excTransD, Action( Simultaneous [ RWM(Match ANY, Write B, Here) ; RWM(Match(VAL B), No_Write, Right) ; Nop ]), excTrans_m) ;
+      (excTransD, Action( Simultaneous [ RWM(Match ANY, Write B, Here) ; RWM(Match(VAL B), No_Write, Right) ; Nop ]), excTrans_m) ;
+      (excTrans_m, Action( Simultaneous [ RWM(Match ANY, No_Write, Left) ; RWM(Match(VAL L), No_Write, Right) ; Nop ]), excTransA) ;
+      (excTrans_m, Action( Simultaneous [ RWM(Match ANY, No_Write, Right) ; RWM(Match(VAL R), No_Write, Right) ; Nop ]), excTransA) ;
+      (excTrans_m, Action( Simultaneous [ RWM(Match ANY, No_Write, Here) ; RWM(Match(VAL H), No_Write, Right) ; Nop ]), excTransA) ;
+
+      (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Acc), No_Write, Right); Nop]), accept);
+      (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Exc), No_Write, Right); Nop]), reject);
+      (chEtatD, Action(Simultaneous [Nop; RWM(Match(VAL Std), No_Write, Right); Nop]), chEtat1);
+      (chEtat1, Action(Simultaneous [Nop; RWM(Match(BUT S), No_Write, Right); Nop]), reject);
+      (chEtat1, Action(Simultaneous [Nop; RWM(Match(VAL S), No_Write, Right); Nop]), chEtat2);
+      (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL Z), No_Write, Right); RWM(Match ANY, Write Z, Right)]), chEtat2);
+      (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL U), No_Write, Right); RWM(Match ANY, Write U, Right)]), chEtat2);
+      (chEtat2, Action(Simultaneous[Nop; RWM(Match(VAL C), No_Write, Here); Nop]), chEtatR);
+      (chEtatR, Run_on(TM_Basic.left_most, [3]), chEtatA) ;
+
+      (loopD, Action( Simultaneous [ Nop ; RWM(Match ANY, No_Write, Right) ; Nop ] ), rechTransD) ;
+      (rechTransA, Action( Simultaneous [ Nop ; Nop ; Nop ] ), excTransD) ;
+      (excTransA, Action( Simultaneous [ Nop ; RWM(Match ANY, No_Write, Right) ; Nop ] ), chEtatD) ;
+      (chEtatA, Parallel [ Action(Nop) ; Run(TM_Basic.left_most) ; Action(Nop) ], loopD) ;
+
+      (init, Parallel [ Run(TM_Basic.left_most) ; Run(TM_Basic.left_most) ; Action(Nop) ], std1) ;
+      (std1, Action( Simultaneous [ Nop ; RWM(Match ANY, No_Write, Right) ; Nop ] ), chEtatD)
 		]
 	}
 
